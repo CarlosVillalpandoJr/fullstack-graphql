3f71597e8141f1a23f7053553e6c7af0807c6f96
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-06-17 16:55:13 -0500

    installed dependencies and setting up Type Defs/Resolvers/Server

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,21 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, __, context) {
+      return context.models.Pet.findMany()
+    } 
   },
-  Mutation: {
+  // Mutation: {
     
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
     
-  }
+  // }
 }
